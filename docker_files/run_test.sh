07ecc3c66c2b9865a8d44710990e99248e5d5177
Jordi Riera <jordi.riera@numigi.com>
2018-03-15 16:56:12 -0400

    force the db_host to be db

--- docker_files/run_test.sh
+++ docker_files/run_test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-odoo -r odoo -w odoo -d odoo -i base --stop-after-init --test-enable --workers 0
+odoo --db_host=db -r odoo -w odoo -d odoo -i base --stop-after-init --test-enable --workers 0
