64e72822bf41d990233bc52c8c00b65d6487232a
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2019-05-02 22:05:25 -0400

    add variables (TA#11537) (#44)

--- docker_files/extended_entrypoint.sh
+++ docker_files/extended_entrypoint.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -e
-until pg_isready --host=db; do
-    echo "$(date) - waiting for postgres...on db"
-    sleep 1
-done
+
+# allow to mentioned if the sleep should be operated.
+# the wait is useful when the db server is started at the docker-compose up
+# useless for hosted servers.
+wait_for_db_host=${WAIT_FOR_DB_HOST:-true}
+
+if [[ "${wait_for_db_host}" = true ]] ; then
+
+    # allow to use a different name for the host of the db than "db"
+    # useful when multiple odoo stacks are deployed on the same machine
+    db_host=${DB_HOST:-db}
+
+    until pg_isready --host=${db_host}; do
+        echo "$(date) - waiting for postgres...on ${db_host}"
+        sleep 1
+    done
+fi
 
 exec /entrypoint.sh "$@"
