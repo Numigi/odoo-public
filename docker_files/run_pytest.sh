3ab7cf62e0b5503a59d97e3ac7725751692cc6fe
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-05-22 21:01:09 +0100

    comment coverage options

--- docker_files/run_pytest.sh
+++ docker_files/run_pytest.sh
@@ -2,18 +2,13 @@
 LOG_DIR=${LOG_ODOO:-/var/log/odoo}
 TEST_DIR=${1:-/mnt/extra-addons}
 
-mkdir -p $LOG_DIR
-chown -R odoo:odoo $LOG_DIR
+cd $LOG_DIR
 
-
-cd $TEST_DIR
-
-# Run pytest with the specified directories and options
-pytest . \
+pytest $TEST_DIR \
     -v \
     --disable-warnings \
-    --cov \
-    --cov-branch \
-    --cov-config /.coveragerc \
-    --cov-report xml \
-    --junit-xml=${LOG_DIR}/junit.xml
+#    --cov \
+#    --cov-branch \
+#    --cov-config /.coveragerc \
+#    --cov-report xml \
+#    --junit-xml=${LOG_DIR}/junit.xml
